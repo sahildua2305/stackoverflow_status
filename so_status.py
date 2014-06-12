065f91d46f690defe5edd18a94646228efebfbcf
Sahil Dua <sahildua2305@gmail.com>
2014-06-12 13:55:58 +0530

    Create so_status.py

---
+++ so_status.py
@@ -0,0 +1,25 @@
+import urllib2
+import smtplib
+from bs4 import BeautifulSoup
+
+def check(found):
+    url = urllib2.urlopen("http://stackoverflow.com/questions/24178980/how-to-monitor-vlc-player-on-windows-7-using-python")
+    data = url.read()
+    soup = BeautifulSoup(data)
+    if soup.find('div', class_="answers-subheader").h2.__str__() != "<h2>\n</h2>":
+        message = "There is an answer"
+        try:
+            smtpObj = smtplib.SMTP('127.0.0.1', 25, 'localhost')
+            smtpObj = smtplib.SMTP('localhost')
+            smtpObj.sendmail(sender, receivers, message)
+            print "Email Sent!!!"
+        except:
+            print "Error: Unable to send email!!!"
+        return "Yes"
+    print "There is no answer as of now"
+    return "No"
+
+if __name__ == '__main__':
+    found = "No"
+    while found == "No":
+        found = check(found)
